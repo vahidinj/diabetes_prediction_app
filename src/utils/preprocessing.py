fd78b1c0805a5c46e824500de27248fd60deed7c
Vahidin Jupic <162918876+vahidinj@users.noreply.github.com>
2025-04-08 22:57:21 -0400

    Update preprocessing.py

--- src/utils/preprocessing.py
+++ src/utils/preprocessing.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 df = pd.read_csv(
-    "/Users/deanjupic/Documents/Python/Projects/diabetes_prediction_app/Data/diabetes.csv"
+    "Data/diabetes.csv"
 )
 
 df = df.drop(["BloodPressure", "SkinThickness"], axis=1)
